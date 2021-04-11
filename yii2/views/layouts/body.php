8dd69d745a0807f938f2ad9234d1b062833dbb39
chanleo111 <chanleo000@gmail.com>
2021-04-11 17:10:43 +0800

    update layout

---
+++ yii2/views/layouts/body.php
@@ -0,0 +1,13 @@
+<?php
+use app\widgets\Alert;
+use yii\widgets\Breadcrumbs;
+
+?>
+
+<div class="container">
+    <?= Breadcrumbs::widget([
+        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+    ]) ?>
+    <?= Alert::widget() ?>
+    <?= $content ?>
+</div>
