df7cd3861198880814e09641d4c70f381e3f7a96
chanleo111 <chanleo000@gmail.com>
2021-04-14 18:27:07 +0800

    disable debug mode

--- yii2/config/web.php
+++ yii2/config/web.php
@@ -13,7 +13,7 @@ $config = [
     ],
     'components' => [
         'request' => [            
-            'cookieValidationKey' => 'qwqw12131212',
+            'cookieValidationKey' => '636dfc2afd553c9d9d71105e8713af08',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -53,18 +53,20 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
+    /*
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
         //'allowedIPs' => ['127.0.0.1', '::1'],
     ];
-
+    */
+    
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 }
 
